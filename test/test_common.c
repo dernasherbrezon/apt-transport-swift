71b510a714080f58cbc32af73ad239551d145a6c
dernasherbrezon <rodionovamp@mail.ru>
2019-12-30 11:04:56 +0300

    fix memory test

--- test/test_common.c
+++ test/test_common.c
@@ -38,7 +38,9 @@ START_TEST (test_trim) {
 END_TEST
 
 START_TEST (test_concat) {
-	ck_assert_str_eq("api.example.com/v3/auth/tokens", concat("api.example.com", "/v3/auth/tokens"));
+	const char* actual = concat("api.example.com", "/v3/auth/tokens");
+	ck_assert_str_eq("api.example.com/v3/auth/tokens", actual);
+	free(actual);
 }
 END_TEST
 
